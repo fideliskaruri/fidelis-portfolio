61cea6802204c5a1b6b36b76e330532f74ceb561
fideliskaruri <fideliskaruri@gmail.com>
2024-10-31 20:18:44 +0300

    making sure csr is enabled for all components

--- app/components/Cursor.tsx
+++ app/components/Cursor.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+'use client';
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { isMobile } from 'react-device-detect';
@@ -22,7 +21,7 @@ const Cursor = () => {
     return (
         <motion.div
             className="fixed w-12 h-12 rounded-full pointer-events-none bg-white opacity-80 z-50 border border-black mix-blend-difference"
-            
+
             animate={{
                 x: mousePosition.x - 48, // Offset to center the effect
                 y: mousePosition.y - 48,
